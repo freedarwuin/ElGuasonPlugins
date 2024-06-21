0a9af39b3b0eaa5710a63793ff2cf2df1bd77eb1
Andrew <andjruben@gmail.com>
2024-06-21 14:58:45 -0500

    feat: add ironbuddy plugin

---
+++ IronBuddy/src/main/java/com.spinplugins.IronBuddy/IronBuddyOverlay.java
@@ -0,0 +1,54 @@
+package com.spinplugins.IronBuddy;
+
+import com.example.EthanApiPlugin.Collections.TileObjects;
+import net.runelite.api.*;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.ui.overlay.*;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.Optional;
+
+public class IronBuddyOverlay extends OverlayPanel {
+    private final Client client;
+    private final IronBuddyConfig config;
+    private final IronBuddyPlugin plugin;
+
+    @Inject
+    private IronBuddyOverlay(Client client, IronBuddyPlugin plugin, IronBuddyConfig config) {
+        this.client = client;
+        this.plugin = plugin;
+        this.config = config;
+        setPosition(OverlayPosition.DYNAMIC);
+        setLayer(OverlayLayer.ABOVE_SCENE);
+    }
+
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        panelComponent.setPreferredSize(new Dimension(240, 360));
+        panelComponent.getChildren().add(TitleComponent.builder()
+                .text("IronBuddy")
+                .color(new Color(214, 143, 49))
+                .build());
+        panelComponent.getChildren().add(LineComponent.builder()
+                .left("Script:")
+                .right(config.taskType().name())
+                .build());
+        panelComponent.getChildren().add(LineComponent.builder()
+                .left("Executing task:")
+                .right(plugin.getActiveTaskName() == null ? "None" : plugin.getActiveTaskName())
+                .build());
+        if(plugin.timeout > 0) {
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left("Tick timeout:")
+                    .right(Integer.toString(plugin.timeout))
+                    .build());
+        }
+
+        return super.render(graphics);
+    }
+}
