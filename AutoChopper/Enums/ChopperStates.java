793c0025ebba24b8b43af56016084e442dfd1e65
ElGuason <darwuinjose@gmail.com>
2024-08-07 18:35:55 -0400

    Actualización

---
+++ AutoChopper/Enums/ChopperStates.java
@@ -0,0 +1,12 @@
+package net.runelite.client.plugins.AutoChopper.Enums;
+
+public enum ChopperStates {
+    STARTING,
+    CHOPPING_LOGS,
+    BURNING_LOGS,
+    DROPPING_LOGS,
+    OPENING_BANK,
+    CLOSING_BANK,
+    BANKING_LOGS,
+    USING_SPECIAL_ATTACK
+}
